11bdf6ef47d1250b7b57b85195ba2af989e0e3ad
Anders Evenrud <andersevenrud@gmail.com>
2011-06-23 04:05:13 +0200

    Crash dialog

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -85,7 +85,15 @@
             application._uuid  = data.result.uuid;
 
             setTimeout(function() {
-              application.run();
+              try {
+                application.run();
+              } catch ( ex ) {
+                try {
+                  _Desktop.addWindow(new CrashDialog(application, ex.message, ex.stack));
+                } catch ( ee ) {
+                  API.system.dialog("error", "Application '" + app_name + "' has crashed with error '" + ex + "'!");
+                }
+              }
             }, 100);
           } else {
             var error = "Application Script not found!";
@@ -2372,9 +2380,13 @@
 
       if ( height ) {
         el.css("height", (height) + "px");
+      } else {
+        el.css("height", (this._height) + "px");
       }
       if ( width ) {
         el.css("width", (width) + "px");
+      } else {
+        el.css("width", (this._width) + "px");
       }
     },
 
@@ -2678,6 +2690,81 @@
         self.cmd_cancel();
       });
     }
+
+  });
+
+  /////////////////////////////////////////////////////////////////////////////
+  // CRASH DIALOG
+  /////////////////////////////////////////////////////////////////////////////
+
+  var CrashDialog = Window.extend({
+
+    init : function(app, error, trace) {
+      var title = sprintf("Application '%s' crashed!", app._name);
+
+      this._super("Crash", false);
+      this._content = "<div class=\"Crash\"><span>" + title + "</span><div class=\"error\"><div><b>Error</b></div><textarea></textarea></div><div class=\"trace\"><div><b>Trace</b></div><textarea></textarea></div></div>";
+      this._title = title;
+      this._icon = 'status/software-update-urgent.png';
+      this._is_draggable = true;
+      this._is_resizable = false;
+      this._is_scrollable = false;
+      this._is_sessionable = false;
+      this._is_minimizable = true;
+      this._is_maximizable = false;
+      this._is_closable = true;
+      this._is_orphan = false;
+      this._is_ontop = true;
+      this._width = 600;
+      this._height = 300;
+      this._gravity = "center";
+
+      this.error = error;
+      this.trace = trace;
+    },
+
+
+    create : function(id, mcallback) {
+     var self = this;
+      var el = this._super(id, mcallback);
+
+      $(el).find(".Crash").css({
+        "position" : "absolute",
+        "top" : "5px",
+        "left" : "5px",
+        "right" : "5px",
+        "bottom" : "5px"
+      });
+      $(el).find(".Crash span").css({
+        "font-weight" : "bold"
+      });
+
+      $(el).find(".error").css({
+        "position" : "absolute",
+        "top" : "20px",
+        "left" : "0px",
+        "right" : "0px",
+        "bottom" : "140px"
+      }).find("textarea").val(self.error);
+
+      $(el).find(".trace").css({
+        "position" : "absolute",
+        "top" : "120px",
+        "left" : "0px",
+        "right" : "0px",
+        "bottom" : "0px"
+      }).find("textarea").val(self.trace);
+
+      $(el).find("textarea").css({
+        "resize" : "none",
+        "position" : "absolute",
+        "top" : "15px",
+        "left" : "0px",
+        "right" : "0px",
+        "bottom" : "0px"
+      });
+    }
+
 
   });
 
