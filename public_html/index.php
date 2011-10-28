a629f0e7dbc5091037140ad364745f8092f75139
Anders Evenrud <andersevenrud@gmail.com>
2011-10-28 21:29:29 +0200

    Added GA

--- public_html/index.php
+++ public_html/index.php
@@ -217,6 +217,18 @@ header("Content-Type: text/html; charset=utf-8");
   <script type="text/javascript" src="/?resource=dialog.rename.js"></script>
   <script type="text/javascript" src="/?resource=dialog.copy.js"></script>
   <script type="text/javascript" src="/?resource=dialog.color.js"></script>
+
+  <script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-26635797-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  </script>
 </head>
 <body>
 
