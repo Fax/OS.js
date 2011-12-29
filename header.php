d285bff9bdc2b7323f77812858455cc3d07ce810
Anders Evenrud <andersevenrud@gmail.com>
2011-12-29 07:58:28 +0100

    Version bump

--- header.php
+++ header.php
@@ -24,7 +24,7 @@ ini_set("upload_max_filesize",              "256M");
 
 define("PROJECT_AUTHOR",    "Anders Evenrud");
 define("PROJECT_CONTACT",   "andersevenrud@gmail.com");
-define("PROJECT_VERSION",   "0.5-beta8"); // Next: 0.6
+define("PROJECT_VERSION",   "0.5-beta9"); // Next: 0.6
 define("PROJECT_CODENAME",  "FoxTrot"); // Next: DiscoFox
 define("PROJECT_HOST",      (php_uname('n')));
 
