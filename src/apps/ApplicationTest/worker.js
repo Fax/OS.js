3ebb181ab289aea03d7f687ccc4a0940ed003157
Anders Evenrud <andersevenrud@gmail.com>
2012-01-22 05:56:36 +0100

    Changed license from GPL to BSD

--- src/apps/ApplicationTest/worker.js
+++ src/apps/ApplicationTest/worker.js
@@ -3,7 +3,7 @@
  * @file
  * worker.test.js
  * @author Anders Evenrud <andersevenrud@gmail.com>
- * @license GPLv3 (see http://www.gnu.org/licenses/gpl-3.0.txt)
+ * @licence Simplified BSD License
  * @created 2011-12-21
  */
 
