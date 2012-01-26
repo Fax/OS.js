95ebd59c4731d3ef926602187576a4ce78aa5686
Anders Evenrud <andersevenrud@gmail.com>
2012-01-26 03:07:39 +0100

    Reverted app-gen change

--- src/Application.class.php
+++ src/Application.class.php
@@ -149,10 +149,6 @@ abstract class Application
             continue;
           }
 
-          if ( !$app_enabled && ENV_PRODUCTION ) {
-            continue;
-          }
-
           $windows   = Array();
           $resources = Array();
           $mimes     = Array();
