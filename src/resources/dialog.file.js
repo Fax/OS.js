4aed8bc5b13cb7f2d9a0525d9fd0db80fdb8614d
Anders Evenrud <andersevenrud@gmail.com>
2012-01-26 02:17:14 +0100

    Fixed icon bug in DialogFile

--- src/resources/dialog.file.js
+++ src/resources/dialog.file.js
@@ -85,7 +85,11 @@ OSjs.Dialogs.FileOperationDialog = (function($, undefined) {
                 if ( result.hasOwnProperty(f) ) {
                   var o = result[f];
                   var el = $("<li><img alt=\"\" src=\"/img/blank.gif\" /><span></span></li>");
-                  el.find("img").attr("src", "/img/icons/16x16/" + o.icon);
+                  if ( o.icon.match(/^\//) ) {
+                    el.find("img").attr("src", o.icon);
+                  } else {
+                    el.find("img").attr("src", "/img/icons/16x16/" + o.icon);
+                  }
                   el.find("span").html(f);
                   el.addClass(i % 2 ? "odd" : "even");
                   if ( o['protected'] == "1" ) {
