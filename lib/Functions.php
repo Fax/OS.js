c35dce5a2290061d04b6019fea6f35f2975b2aaa
Anders Evenrud <andersevenrud@gmail.com>
2012-02-22 15:07:41 +0100

    Fixed a Function

--- lib/Functions.php
+++ lib/Functions.php
@@ -103,9 +103,12 @@ function array_merge_deep($arr) {
  * @return String
  */
 function getHumanSize($bytes) {
-  $symbols = array('B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB');
-  $exp = floor(log($bytes)/log(1024));
-  return sprintf('%.2f '.$symbols[$exp], ($bytes/pow(1024, floor($exp))));
+  if ( $bytes ) {
+    $symbols = array('B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB');
+    $exp = floor(log($bytes)/log(1024));
+    return sprintf('%.2f '.$symbols[$exp], ($bytes/pow(1024, floor($exp))));
+  }
+  return "0 B";
 }
 
 ?>
