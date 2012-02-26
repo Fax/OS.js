a4494fb369277d05b4f97880e393679935712c89
Anders Evenrud <andersevenrud@gmail.com>
2012-02-26 17:31:30 +0100

    Automatic: header.php stamp update

--- header.php
+++ header.php
@@ -40,7 +40,7 @@ define("PROJECT_CONTACT",   "andersevenrud@gmail.com");
 define("PROJECT_VERSION",   "0.6-alpha6"); // Next: 0.7
 define("PROJECT_CODENAME",  "DiscoFox"); // Next: ???
 define("PROJECT_HOST",      (php_uname('n')));
-define("PROJECT_BUILD",     "2bb0f71");
+define("PROJECT_BUILD",     "a63e72f");
 define("PROJECT_MODIFIED",  "1330273890");
 
 //
