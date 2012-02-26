bb36dc340b79ccba3b79a917302a7976078d1adf
Anders Evenrud <andersevenrud@gmail.com>
2012-02-26 16:33:50 +0100

    Updated header

--- header.php
+++ header.php
@@ -40,8 +40,8 @@ define("PROJECT_CONTACT",   "andersevenrud@gmail.com");
 define("PROJECT_VERSION",   "0.6-alpha6"); // Next: 0.7
 define("PROJECT_CODENAME",  "DiscoFox"); // Next: ???
 define("PROJECT_HOST",      (php_uname('n')));
-define("PROJECT_BUILD",     "975c044");
-define("PROJECT_MODIFIED",  "1330269797");
+define("PROJECT_BUILD",     "6216a1a");
+define("PROJECT_MODIFIED",  "1330270430");
 
 //
 // Environment
