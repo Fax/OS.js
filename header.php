f82e7796c544b36f3fedde3a135e46aa4b3e0a15
Anders Evenrud <andersevenrud@gmail.com>
2012-04-22 15:39:55 +0200

    [AUTO]: header.php update

--- header.php
+++ header.php
@@ -58,8 +58,8 @@ define("PROJECT_CONTACT",   "andersevenrud@gmail.com");
 define("PROJECT_VERSION",   "0.7.002");
 define("PROJECT_CODENAME",  "Catwalk");
 define("PROJECT_HOST",      (php_uname('n')));
-define("PROJECT_BUILD",     "d406880");
-define("PROJECT_MODIFIED",  "1335101483");
+define("PROJECT_BUILD",     "f87b90f");
+define("PROJECT_MODIFIED",  "1335101995");
 
 //
 // Environment
