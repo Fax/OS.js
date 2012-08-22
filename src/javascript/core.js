77fc7c540c4d4777e4e12bcc3681c2cc7da3a461
Anders Evenrud <andersevenrud@gmail.com>
2012-08-22 16:01:07 +0200

    Core: user.session.appstorage is not stored in server anymore (2)

--- src/javascript/core.js
+++ src/javascript/core.js
@@ -66,6 +66,7 @@
   var ENV_CACHE              = undefined;           //!< Server-side cache enabled state
   var ENV_PRODUCTION         = undefined;           //!< Server-side production env. state
   var ENV_DEMO               = undefined;           //!< Server-side demo env. state
+  var STORE_APPS             = false;               //!< Always store applicaion data in registry
   // @endconstants
 
   /**
@@ -3831,7 +3832,7 @@
     getRegistry : function() {
       var exp = {};
       for ( var i in this._registry ) {
-        if ( i == "user.session.appstorage" )
+        if ( i == "user.session.appstorage" && !STORE_APPS )
           continue;
 
         exp[i] = this._get(i, in_array(this._registry[i], ["list", "array"]));
