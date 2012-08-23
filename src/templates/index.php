4155ce5ac3da62c30cd77fcf4080b9272ee50729
Anders Evenrud <andersevenrud@gmail.com>
2012-08-23 19:51:52 +0200

    Template: Google author

--- src/templates/index.php
+++ src/templates/index.php
@@ -138,13 +138,13 @@ foreach ( CoreSettings::getPreload() as $key => $links ) {
 </head>
 <body class="env_<?php print (ENV_DEMO ? "demo" : (ENV_PRODUCTION ? "live" : "dev")); ?>">
 
-<div id="HomePage" style="display:none;">
+<div id="HomePage">
   <h1>OS.js - JavaScript Web Desktop Environment (Version <?php print PROJECT_VERSION; ?> demo)</h1>
   <p>
     This is the demo site for testing OS.js releases.
   </p>
   <p>
-    For more information contact the author: Anders Evenrud <a href="mailto:andersevenrud@gmail.com">&lt;andersevenrud@gmail.com&gt;</a>
+    For more information contact the author: <a href="https://plus.google.com/101576798387217383063?rel=author">Anders Evenrud</a> <a href="mailto:andersevenrud@gmail.com">&lt;andersevenrud@gmail.com&gt;</a>
   </p>
   <p>
     Homepage on <a href="http://andersevenrud.github.com/OS.js/">GitHub</a>
