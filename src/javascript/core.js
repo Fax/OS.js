5b3b244623771c037b42ae021e256cc2a7658e59
Anders Evenrud <andersevenrud@gmail.com>
2012-08-27 19:10:49 +0200

    Core\JS: Fixed sticky ALT/CTRL/SHIFT keys when loosing focus

--- src/javascript/core.js
+++ src/javascript/core.js
@@ -2263,6 +2263,8 @@
       if ( this.running ) {
         console.log("Unbinding events...");
 
+        $(window).unbind("focus",                       this.global_focus);
+        $(window).unbind("blur",                        this.global_blur);
         //$(window).unbind("offline",                   this.global_offline);
         $(document).unbind("keydown",                 this.global_keydown);
         //$(document).unbind("keypress",                this.global_keypress);
@@ -2579,6 +2581,8 @@
       OSjs.Classes.ProgressBar($("#LoadingBar"), 40);
 
       // Bind global events
+      $(window).bind("focus",         this.global_focus);
+      $(window).bind("blur",          this.global_blur);
       $(window).bind("beforeunload",  this.leaving);
       //$(window).bind("offline",       this.global_offline);
       $(document).bind("keydown",     this.global_keydown);
@@ -2786,6 +2790,24 @@
           console.log("Core::global_offline()", _OnLine);
         }
       }
+    },
+
+    /**
+     * Core::global_focus() -- When browser gets focus
+     * @param   DOMEvent    ev      DOM Event
+     * @return  void
+     */
+    global_focus : function(ev) {
+      KEY_ALT = KEY_SHIFT = KEY_CTRL = false;
+    },
+
+    /**
+     * Core::global_blur() -- When browser gets blurred
+     * @param   DOMEvent    ev      DOM Event
+     * @return  void
+     */
+    global_blur : function(ev) {
+      KEY_ALT = KEY_SHIFT = KEY_CTRL = false;
     },
 
     /**
