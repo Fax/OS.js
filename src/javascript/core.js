6f14338b06f0909d8185d14e15ac906bdc32478d
Anders Evenrud <andersevenrud@gmail.com>
2012-08-29 16:51:22 +0200

    VFS: Added error message when no result

--- src/javascript/core.js
+++ src/javascript/core.js
@@ -2768,7 +2768,9 @@
      * @return  bool
      */
     global_error : function(msg, url, lno) {
+      url = url || window.location.href;
       var title = msg.split(":", 1).shift();
+
       CrashCustom("_" + title, msg, sprintf("in %s on line %d", url, lno));
 
       if ( ENV_BUGREPORT ) {
