8e87736392db7ef74eafcf95c83313470ca23314
Anders Evenrud <andersevenrud@gmail.com>
2012-08-29 03:44:43 +0200

    count-lines: Added more folders

--- bin/count-lines.sh
+++ bin/count-lines.sh
@@ -5,14 +5,16 @@
 
 echo "Core:"
 echo -n "  JS Base:       "; cat src/javascript/*.js | wc -l
+echo -n "  JS Locales:    "; cat src/javascript/locale/*/*.js | wc -l
 echo -n "  CSS Base:      "; cat src/javascript/*.css | wc -l
 echo -n "  PHP Sources:   "; cat src/*.php | wc -l
 echo -n "  PHP Libs:      "; cat lib/*.php | wc -l
 echo -n "  PHP Public:    "; cat header.php public_html/*.php | wc -l
+echo -n "  Templates:     "; cat src/templates/*.* | wc -l
 
 echo ""
 
-TOTAL=`cat src/javascript/*.js src/javascript/*.css src/*.php lib/*.php header.php public_html/*.php | wc -l`
+TOTAL=`cat src/javascript/*.js src/javascript/*.css src/*.php lib/*.php header.php public_html/*.php src/templates/*.* src/javascript/locale/*/*.js | wc -l`
 echo "Total: $TOTAL (Excluding Vendor files)"
 
 echo ""
