8403b32760211999e83d5fdb9e538a7c2dd5b898
Anders Evenrud <andersevenrud@gmail.com>
2012-09-03 19:25:43 +0200

    Core\JS: Fixed desktop contextmenu

--- src/javascript/core.js
+++ src/javascript/core.js
@@ -5798,6 +5798,13 @@
       return this._super(ev, el, item);
     },
 
+    onContextMenu : function(ev, el) {
+      if ( _Desktop )
+        _Desktop.showContextMenu(ev);
+
+      return false;
+    },
+
     onItemContextMenu : function(ev, el, item) {
       var self = this;
       var result = this._super(ev, el, item);
